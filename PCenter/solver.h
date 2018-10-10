c5ebf9cd705e6f3b7752385a2c5d3b5493f1aea9
zjl9959 <zjluestc@outlook.com>
2018-10-10 15:36:08 +0800

    first commit, create project PCenter [VS2017_v141]

---
+++ PCenter/solver.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef PCENTER_SOLVER_H
+#define PCENTER_SOLVER_H
+
+class Solver {
+public:
+    Solver() {}
+    ~Solver() {}
+protected:
+    
+private:
+
+};
+
+
+#endif // !PCENTER_SOLVER_H
