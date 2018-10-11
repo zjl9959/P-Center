f688c585337153c70edfc37b1053af3fff2b5339
zjl9959 <zjluestc@outlook.com>
2018-10-11 17:19:42 +0800

    add solver

--- PCenter/solver.cc
+++ PCenter/solver.cc
@@ -1,3 +1,67 @@
 #include "solver.h"
+#include <string>
+#include <fstream>
+#include <iostream>
 
+using namespace std;
 
+namespace pcenter_solver {
+
+void Solver::LoadGraph(char * path) {
+    string strpath(path);
+    if (strpath.rfind(".txt") != string::npos) {
+        instance_type_ = InstanceType::TXT;
+        //first line:<node_number> <edge_number>; other lines:<node1> <node2> <distance>
+        ifstream ifs(path);
+        if (!ifs.is_open()) {
+            cout << "ERROR:Can't open instance file!" << endl;
+            return;
+        } else {
+            double dist = 0.0;
+            int edge_num = 0, node1 = 0, node2 = 0;
+            ifs >> vertex_num_ >> edge_num;
+            //resize graph and set it's default value as 0
+            graph_.resize(vertex_num_);
+            for (int i = 0; i < vertex_num_; ++i)
+                graph_[i].resize(vertex_num_);
+            for (int i = 0; i < vertex_num_; ++i)
+                for (int j = 0; j < vertex_num_; ++j)
+                    graph_[i][j] = 0;
+            //save value to graph
+            for (int i = 0; i < edge_num; ++i) {
+                ifs >> node1 >> node2 >> dist;
+                graph_[node1 - 1][node2 - 1] = dist;  //node id in the instance start from 1
+                graph_[node2 - 1][node1 - 1] = dist;
+            }
+            ifs.close();
+        }
+    } else if (strpath.rfind(".tsp") != string::npos) {
+        instance_type_ = InstanceType::TSP;
+        //first line:<node_number>; other lines:<node> <position_x> <position_y>
+        ifstream ifs(path);
+        if (!ifs.is_open()) {
+            cout << "ERROR:Can't open instance file!" << endl;
+            return;
+        } else {
+            int node = 0;
+            double pos_x = 0.0, pos_y = 0.0;
+            ifs.close();
+        }
+    }
+}
+
+int Solver::Solve() {
+    return 0;
+}
+
+std::vector<int> Solver::GetResult() {
+    return std::vector<int>();
+}
+
+void Solver::Init() {}
+
+bool Solver::Check() {
+    return false;
+}
+
+}
--- PCenter/solver.h
+++ PCenter/solver.h
@@ -2,15 +2,44 @@
 #ifndef PCENTER_SOLVER_H
 #define PCENTER_SOLVER_H
 
+#include <vector>
+#include <map>
+
+namespace pcenter_solver {
+
 class Solver {
 public:
-    Solver() {}
+    enum InstanceType { TSP, TXT };
+public:
+    explicit Solver(int P) :facility_num_(P), vertex_num_(0) {}
     ~Solver() {}
+    void LoadGraph(char *path);
+    int Solve();
+    std::vector<int> GetResult();
 protected:
-    
+    struct FD {
+        int f1;  //Nearest facility identity
+        double d1;  //Nearest facility distance
+        int f2;  //Second nearest facility identity
+        double d2;  //Second nearest facility distance
+        FD(int facility1, double distance1, int facility2, double distance2) :
+            f1(facility1), d1(distance1), f2(facility2), d2(distance2) {}
+    };
+protected:
+    void Init();
+    bool Check();
 private:
-
+    int facility_num_;
+    int vertex_num_;
+    int instance_type_;
+    double best_objval_;  //best objective value
+    std::vector<std::vector<double>> graph_;  //the origin graph form instance
+    std::vector<std::vector<double>> dist_table_;  //the distance table for all the nodes
+    std::vector<std::vector<int>> tabu_table;
+    std::vector<FD> fd_table_;  //facility and distance table for every node
+    std::vector<int> choosed_facility_;
 };
 
+}
 
 #endif // !PCENTER_SOLVER_H
