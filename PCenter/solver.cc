9ef85b225a05b4d09b9cebdea1fdcbcfdcdb691e
zjl9959 <zjluestc@outlook.com>
2018-10-15 21:50:18 +0800

    add Solver.GenInitSolution()

--- PCenter/solver.cc
+++ PCenter/solver.cc
@@ -76,17 +76,51 @@ int Solver::Solve() {
 }
 
 std::vector<int> Solver::GetResult() {
-    return choosed_facility_;
+    return facility_nodes_;
 }
 
 void Solver::GenInitSolution() {
+    //sort the neighbors by distance for every node
+    dist_table_.resize(vertex_num_);
+    vector<Edge> neighbors;
+    for (int i = 0; i < vertex_num_; ++i) {
+        neighbors.clear();
+        for (int j = 0; j < vertex_num_; ++j) {
+            if (i != j && graph_matrix_[i][j] != kINF)
+                neighbors.push_back(Edge(j, graph_matrix_[i][j]));
+        }
+        sort_heap(neighbors.begin(),neighbors.end(), [](Edge &lhs, Edge &rhs) {
+            return lhs.dist < rhs.dist; });
+        dist_table_.push_back(neighbors);
+    }
     int choosed_facility_num = 0;
-    fd_table_.resize(vertex_num_);
-    choosed_facility_.resize(facility_num_);
+    facility_nodes_.resize(facility_num_);
+    isfacility_.resize(vertex_num_);
     //choose the first center randomly
-    choosed_facility_[choosed_facility_num++] = RandomVertex();
+    facility_nodes_[choosed_facility_num++] = RandomVertex();
+    isfacility_[facility_nodes_[0]] = true;
     while (choosed_facility_num < facility_num_) {
-        //[TODO]choose other centers
+        //get the longest service edge
+        double longest_service_edge = -kINF;
+        int longest_service_node = NONE;
+        for (int u = 0; u < vertex_num_; ++u) {  //u duputy user node identity
+            double service_edge = kINF;
+            if (!isfacility_[u]) {
+                for (int f : facility_nodes_)  //for every user node, choose a facility
+                    if (graph_matrix_[f][u] < service_edge)
+                        service_edge = graph_matrix_[f][u];
+                if (service_edge != kINF && longest_service_edge < service_edge) {
+                    longest_service_edge = service_edge;
+                    longest_service_node = u;
+                }
+            }
+        }
+        //random select a node form the kth nearest neighbor of longest_user_node
+        vector<int> kth_neighbor;
+        for (Edge n : dist_table_[longest_service_node])
+            if (!isfacility_[n.node]) kth_neighbor.push_back(n.node);
+        facility_nodes_[choosed_facility_num++] = kth_neighbor[rand() % kConsiderRange];
+        isfacility_[facility_nodes_[choosed_facility_num - 1]] = true;
     }
     //Init tabu table
     tabu_table_.resize(vertex_num_);
@@ -95,6 +129,28 @@ void Solver::GenInitSolution() {
     for (int i = 0; i < vertex_num_; ++i)
         for (int j = 0; j < vertex_num_; ++j)
             tabu_table_[i][j] = 0;
+    //Init FDTable
+    FDtable_.resize(vertex_num_);
+    vector<Edge> edge_pair(2);
+    for (int i = 0; i < vertex_num_; ++i) {
+        edge_pair.clear();  //must be clear
+        GetKthNeighbors(i, 2, edge_pair);
+        if (isfacility_[i])
+            FDtable_[i] = FDPair(i, 0, edge_pair[0].node, edge_pair[0].dist);
+        else
+            FDtable_[i] = FDPair(edge_pair[0].node, edge_pair[0].dist, edge_pair[1].node, edge_pair[1].dist);
+    }
+}
+
+//GetKthNeighbor who is not facility, res should be empty
+void Solver::GetKthNeighbors(int node, int k, vector<Edge>& res) {
+    int index = 0;
+    while (k--) {
+        while (isfacility_[dist_table_[node][index++].node]) {
+            if (index == vertex_num_)return;
+        }
+        res.push_back(dist_table_[node][index - 1]);
+    }
 }
 
 bool Solver::Check() {
--- PCenter/solver.h
+++ PCenter/solver.h
@@ -8,47 +8,49 @@
 
 namespace pcenter_solver {
 
+#define NONE -1
+
 class Solver {
 public:
     enum InstanceType { TSP, TXT };
-    const double kINF = 1000000.0;
+    const double kINF = INT32_MAX;
+    const int kConsiderRange = 3;
+    const unsigned int kRandomSeed;
 public:
-    explicit Solver(int P = 0, unsigned int seed = 0) :facility_num_(P), vertex_num_(0) {
-        if (seed == 0) {  //set random seed
-            unsigned int time_now = time(NULL);
-            random_seed_ = time_now / ((time_now << 24) >> 24);
-        } else {
-            random_seed_ = seed;
-        }
-        srand(random_seed_);
-    }
+    explicit Solver(int P = 0, unsigned int seed = 99995011 / (time(NULL) % 255) + 1) :
+        facility_num_(P), vertex_num_(0), kRandomSeed(seed) { srand(kRandomSeed); }
     ~Solver() {}
     void LoadGraph(const char *path);
     int Solve();
     std::vector<int> GetResult();
 protected:
-    struct FD {
-        int f1;  //Nearest facility identity
-        double d1;  //Nearest facility distance
-        int f2;  //Second nearest facility identity
-        double d2;  //Second nearest facility distance
-        FD(int facility1, double distance1, int facility2, double distance2) :
-            f1(facility1), d1(distance1), f2(facility2), d2(distance2) {}
+    struct Edge {
+        int node;
+        double dist;
+        Edge(int Node = NONE, double distance = INT32_MAX) :node(Node), dist(distance) {}
+    };
+    struct FDPair {  //nearest/second_nearest facility/distance table
+        Edge nearest;
+        Edge second_nearest;
+        FDPair(int nearest_facility = NONE, double nearest_dist = INT32_MAX, int second_nearest_facility = NONE, double second_nearest_dist = INT32_MAX) :
+            nearest(nearest_facility, nearest_dist), second_nearest(second_nearest_facility, second_nearest_dist) {}
     };
 protected:
     void GenInitSolution();
     int RandomVertex() { return rand() % vertex_num_; }
+    void GetKthNeighbors(int node, int k, std::vector<Edge> &res);
     bool Check();  //Check the validity of the solution
 private:
     int facility_num_;
     int vertex_num_;
     int instance_type_;
-    unsigned int random_seed_;
     double best_objval_;  //best objective value
     std::vector<std::vector<double>> graph_matrix_;  //the origin graph form instance
+    std::vector<std::vector<Edge>> dist_table_;
     std::vector<std::vector<int>> tabu_table_;
-    std::vector<FD> fd_table_;  //facility and distance table for every node
-    std::vector<int> choosed_facility_;
+    std::vector<FDPair> FDtable_;  //facility and distance table for every node
+    std::vector<int> facility_nodes_;
+    std::vector<bool> isfacility_;
 };
 
 }
